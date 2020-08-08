d0116eb19272817526ab493ec20a0f751a3249c2
giwi97 <giwanthawijekoon@gmail.com>
2020-08-09 01:38:08 +0530

    Passport middleware for google auth

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const morgan = require("morgan");
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 
 //load config
 
 dotenv.config({ path: "./config/config.env" });
+
+//passport config
+
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -22,6 +28,18 @@ if (process.env.NODE_ENV === "development") {
 
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+// sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// passport middleare
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Static folder
 
