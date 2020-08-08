6cfc1c7cd14ec99e7a26af896fa87ed1fd3bf9dd
giwi97 <giwanthawijekoon@gmail.com>
2020-08-08 18:58:32 +0530

    Added middleware

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const morgan = require("morgan");
+const exphbs = require('express-handlebars')
 
 //load config
 
 dotenv.config({ path: "./config/config.env" });
 
+connectDB();
+
 const app = express();
+
+//logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//handlebars
+
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
