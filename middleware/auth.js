a9cfeeb00c4045e7c82d2916c5358997d6635a74
giwi97 <giwanthawijekoon@gmail.com>
2020-08-14 01:45:39 +0530

    removed ensureauth temporarily

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,13 +10,13 @@ module.exports = {
     },
     ensureGuest: function(req, res, next){
 
-        if(req.isAuthenticated()) {
-            res.redirect('/dashboard')
-        } else {
-
+        if(!req.isAuthenticated()) {
             return next()
+        } else {
+            res.redirect('/dashboard')
+            
 
         }
 
-    }
+    },
 }
