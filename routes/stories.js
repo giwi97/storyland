a3ef57f93be1ba90869b49e8841c87679b34f0aa
giwi97 <giwanthawijekoon@gmail.com>
2020-08-16 02:05:39 +0530

    post request for creating stories

--- routes/stories.js
+++ routes/stories.js
@@ -10,5 +10,19 @@ router.get("/add", ensureAuth, (req, res) => {
   res.render('stories/add');
 });
 
+// @desc add form
+//@router POST /stories
+
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id
+    await Story.create(req.body)
+    res.redirect('/dashboard')
+  } catch (error) {
+    console.error(error)
+    res.render('error/500')
+  }
+});
+
 
 module.exports = router;
